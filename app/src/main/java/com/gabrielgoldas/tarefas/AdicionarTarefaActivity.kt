c966b82c56637563f14eda626de530bdea161b4a
Gabriel Goldas <gabriel.goldas@gmail.com>
2024-09-24 15:50:32 -0300

    crating layout and navigation

---
+++ app/src/main/java/com/gabrielgoldas/tarefas/AdicionarTarefaActivity.kt
@@ -0,0 +1,21 @@
+package com.gabrielgoldas.tarefas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.gabrielgoldas.tarefas.databinding.ActivityAdicionarTarefaBinding
+import com.gabrielgoldas.tarefas.databinding.ActivityMainBinding
+
+class AdicionarTarefaActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityAdicionarTarefaBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView( binding.root )
+    }
+}
